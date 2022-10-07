686b142a3dda8c8a4746d279722290a4be211a34
siddhantkothari <siddhant.kothari@gojek.com>
2022-10-07 13:33:21 +0530

    createed homepage and learned abt scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text('Welcome to 30 days of flutter'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
